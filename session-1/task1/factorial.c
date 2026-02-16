434ca35871751be592f5a7bcfb6acb5757e2e529
lukakamidzorac <sjvj0907@leeds.ac.uk>
2026-02-16 11:54:08 +0000

    end of session 4

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -7,6 +7,16 @@
 
  int main( void ) {
     int f[20];
+    int i;
+
+    f[0] = 1;
+
+    for (i = 1; i < 20; i++) {
+      f[i] = i * f[i - 1];
+    }
+    for (i = 0; i< 20; i++) {
+      printf("%2d! = %d\n", i, f[i]);
+    }
 
     /*
     Code to compute the factorial of each array index
